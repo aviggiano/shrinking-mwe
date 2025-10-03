b0e77a6a05020f85ad8097b4851190f213478974
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-10-03 11:37:23 -0300

    Ref https://github.com/crytic/echidna/pull/1454

--- src/CryticTester.sol
+++ src/CryticTester.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.23;
 
 import "./Counter.sol";
 
 // echidna . --contract CryticTester --config echidna.yaml
 // medusa fuzz
 contract CryticTester is Counter {
-    function invariant() public view returns (bool) {
-        return number != 3;
+    function invariant_1() public view returns (bool) {
+        return number != 1234;
+    }
+
+    function invariant_2() public view returns (bool) {
+        return number != 1337;
     }
 }
